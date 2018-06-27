154bcdd74438a39aea228db0642cb1c1e8003119
Tatyana Scotce <tscotce@gmail.com>
2018-06-27 22:04:47 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,6 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   def build_network
-    # self.network.call_letters
+    #self.network.call_letters
   end
 end
