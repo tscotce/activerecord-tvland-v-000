480c5440743059c0208787b740f5c48233e8a4a9
Tatyana Scotce <tscotce@gmail.com>
2018-06-27 22:06:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   def build_network(c)
-    binding.pry
+    # binding.pry
     self.network.call_letters
   end
 end
